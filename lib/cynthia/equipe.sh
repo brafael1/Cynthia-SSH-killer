e10da276c6fa8f4768722949d17cb4dacc486efe
brafael1 <brunorafael.d3v@gmail.com>
2026-07-10 12:37:34 -0300

    feat: adiciona dados da equipe Cynthia (Sinnoh)
    - Array com 6 pokemons e seus golpes característicos
    - Garchomp, Spiritomb, Roserade, Togekiss, Lucario, Milotic

---
+++ lib/cynthia/equipe.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+CYNTHIA_EQUIPE=(
+    "garchomp|Dragon Rush"
+    "spiritomb|Dark Pulse"
+    "roserade|Energy Ball"
+    "togekiss|Aura Sphere"
+    "lucario|Close Combat"
+    "milotic|Surf"
+)
