79989227fc68d710294278fa8e7453e801f5e266
brafael1 <brunorafael.d3v@gmail.com>
2026-09-23 12:39:25 -0300

    refactor(deploy): enviar apenas as libs usadas pelo killer no payload

--- lib/deploy.sh
+++ lib/deploy.sh
@@ -14,8 +14,10 @@ deploy_remoto() {
     local ip_permitido=$6
 
     echo "→ Enviando o killer para $root_remoto..."
-    ssh_executar "$destino" "rm -rf $root_remoto && mkdir -p $root_remoto"
-    scp_enviar -r "$dir_bin" "$dir_lib" "$destino:$root_remoto/"
+    ssh_executar "$destino" "rm -rf $root_remoto && mkdir -p $root_remoto/bin $root_remoto/lib"
+
+    scp_enviar -r "$dir_bin" "$destino:$root_remoto/"
+    scp_enviar -r "$dir_lib/loader.sh" "$dir_lib/sessions.sh" "$dir_lib/notify.sh" "$dir_lib/cynthia" "$destino:$root_remoto/lib/"
 
     local ambiente_sudo='env PATH="$PATH"'
     local comando_killer="bash $root_remoto/bin/encerrar-sessoes-ssh.sh --ip $ip_permitido"
